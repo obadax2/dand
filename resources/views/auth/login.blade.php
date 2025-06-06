91b0eee5d040225760348d493064e8e43b36dbc9
Obada256 <200sniper200@gmail.com>
2025-06-06 13:05:08 +0300

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,6 +33,6 @@
         </div>
     </form>
 
-   
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -88,8 +88,8 @@ Route::middleware(['auth'])->group(function () {
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register.form');
-Route::post('/register', [RegisterController::class, 'register'])->name('register');
+Route::get('/login', [RegisterController::class, 'showRegistrationForm'])->name('register.form');
+Route::post('/login', [RegisterController::class, 'register'])->name('register');
 Route::get('/verify-code', function () {
     return view('auth.verify_code');
 })->name('verify.code.form');
